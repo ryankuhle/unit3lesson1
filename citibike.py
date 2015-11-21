7fe6e4ccec50ac7fcfd507bfe148aeeba716698f
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-21 17:06:50 -0600

    Import normalized JSON data into DataFrame df

--- citibike.py
+++ citibike.py
@@ -1,5 +1,10 @@
 #Import "requests" package to retrieve data from Internet
 import requests
+#Allow pandas to import JSON data
+from pandas.io.json import json_normalize
 
 #"get" function brings in all data from specified URL
 r = requests.get('http://www.citibikenyc.com/stations/json')
+
+#Normalize JSON data from key "stationBeanList", import into DataFrame "df"
+df = json_normalize(r.json()['stationBeanList'])
