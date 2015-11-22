5c8bb8d6b30e42236e19ca03a0ea794cb98351a3
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-21 18:11:27 -0600

    Move data retrieval, parsing, and table insertion elements to  own  function

--- citibike.py
+++ citibike.py
@@ -1,29 +1,13 @@
-#Import "requests" package to retrieve data from Internet
 import requests
-#Allow pandas to import JSON data
 from pandas.io.json import json_normalize
-#Allow charts and plots
-import matplotlib.pyplot as plt
-#pandas
 import pandas as pd
-#Allow to create database
 import sqlite3 as lite
-# a package with datetime objects
 import time
-# a package for parsing a string into a Python datetime object
 from dateutil.parser import parse
 import collections
 
 
-#"get" function brings in all data from specified URL
-r = requests.get('http://www.citibikenyc.com/stations/json')
 
-#Normalize JSON data from key "stationBeanList", import into DataFrame "df"
-##Note: "stations" was gotten by looking at keys from JSON and determining
-##which was useful
-df = json_normalize(r.json()['stationBeanList'])
-
-#Create database
 con = lite.connect('citi_bike.db')
 cur = con.cursor()
 
@@ -46,19 +30,18 @@ cur = con.cursor()
 #with con:
 #    cur.execute("CREATE TABLE available_bikes ( execution_time INT, " +  ", ".join(station_ids) + ");")
 
-#take the string and parse it into a Python datetime object
-exec_time = parse(r.json()['executionTime'])
+def activityStatus():
+    #
+    r = requests.get('http://www.citibikenyc.com/stations/json')
+    df = json_normalize(r.json()['stationBeanList'])
+    exec_time = parse(r.json()['executionTime'])
+    with con:
+        cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time.strftime('%s'),))
+    id_bikes = collections.defaultdict(int) #defaultdict to store available bikes by station
+    for station in r.json()['stationBeanList']:
+        id_bikes[station['id']] = station['availableBikes']
+    with con:
+        for k, v in id_bikes.iteritems():
+            cur.execute("UPDATE available_bikes SET _" + str(k) + " = " + str(v) + " WHERE execution_time = " + exec_time.strftime('%s') + ";")
 
-with con:
-    cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time.strftime('%s'),))
-
-id_bikes = collections.defaultdict(int) #defaultdict to store available bikes by station
-
-#loop through the stations in the station list
-for station in r.json()['stationBeanList']:
-    id_bikes[station['id']] = station['availableBikes']
-
-#iterate through the defaultdict to update the values in the database
-with con:
-    for k, v in id_bikes.iteritems():
-        cur.execute("UPDATE available_bikes SET _" + str(k) + " = " + str(v) + " WHERE execution_time = " + exec_time.strftime('%s') + ";")
+activityStatus()
